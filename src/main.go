9c9e05b378d9e002e883f060262c4ea5e1c55b7a
Edu Maturana <edu@pop-os.localdomain>
2021-10-19 09:04:56 -0300

    Ciclos en Go

--- src/main.go
+++ src/main.go
@@ -2,34 +2,19 @@ package main
 
 import "fmt"
 
-// Functions CLASE 11
-
-func normalFunction(message string) {
-	fmt.Println(message)
-}
-
-func tripleArgument(m1 string, m2 string, m3 string) {
-	fmt.Println(m1, m2, m3)
-}
-
-func returnValue(a int) int {
-	return a * 2
-}
-
-// Luego de los parametros, se declara lo que retorna tambien
-
-func dobleReturn(a int) (c, d int) {
-	return a, a * 2
-}
+// Ciclos CLASE 13
 
 func main() {
-	normalFunction("Hola mundo")
-	tripleArgument("Wena", "a", "a")
+	// For condicional
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
 
-	value := returnValue(2)
-	fmt.Println(value)
+	// For while
+	counter := 11
+	for counter < 20 {
+		fmt.Println(counter)
+		counter++
+	}
 
-	// El _ es para ignorar un valor, en este caso la func retorna 2
-	value1, _ := dobleReturn(2)
-	fmt.Println(value1)
 }
