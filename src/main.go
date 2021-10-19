9aecd0c810de64be30b933f175e848bd245900fc
Edu Maturana <edu@pop-os.localdomain>
2021-10-19 19:08:49 -0300

    Range, close y select en channels

--- src/main.go
+++ src/main.go
@@ -1,29 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func say(text string, c chan<- string) {
-
-	// Nombre del canal <- lo que ingresaremos a el
+func message(text string, c chan string) {
 	c <- text
 }
 
-// A la inversa con salida, mismo de arriba
-
-// func say(text string, c <-chan string) {
-
-// 	// Nombre del canal <- lo que ingresaremos a el
-// 	text = <- c
-// }
-
 func main() {
-	// Crea chan  datatype limit
-	c := make(chan string, 1)
+	c := make(chan string, 2)
+	c <- "Mensaje1"
+	c <- "Mensaje2"
 
-	fmt.Println("Hello")
+	fmt.Println(len(c), cap(c))
 
-	go say("Bye", c)
+	// Range y close
 
-	// El simbolo de linea 8 inverso, en vez de ingresar dato, devuelve la salida
-	fmt.Println(<-c)
+	// Close, cierra el canal y no recibe mas a pesar de su capacidad
+	close(c)
+
+	// c <- "Mensaje3"
+
+	// Range, para recorrer
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	// Select, para cuando no se que canal respondera primero
+	email1 := make(chan string)
+	email2 := make(chan string)
+	go message("Mensaje1 ", email1)
+	go message("Mensaje2 ", email2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		// En case de que m1, venga del canal email1
+		case m1 := <-email1:
+			fmt.Println("Email recibido de", m1)
+		case m2 := <-email2:
+			fmt.Println("Email recibido de", m2)
+		}
+	}
 }
+
+// Nota: Lo ideal es cerrar un canal si se que no voy a recibir mas datos
