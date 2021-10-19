1593a4da1bedc35785a97e29ebe20af7009bc938
Edu Maturana <edu@pop-os.localdomain>
2021-10-19 15:14:32 -0300

    Stringers

--- src/main.go
+++ src/main.go
@@ -8,35 +8,11 @@ type pc struct {
 	brand string
 }
 
-// Crea un metodo ping() dentro del struct myPC, instanciado de pc
-func (myPC pc) ping() {
-	fmt.Println(myPC.brand, "Pong")
-}
-
-func (myPC *pc) duplicateRAM() {
-	myPC.ram = myPC.ram * 2
+func (myPC pc) String() string {
+	return fmt.Sprintf("Tengo %d GBs de RAM , %d GBs de disco y es una %s", myPC.ram, myPC.disk, myPC.brand)
 }
 
 func main() {
-	a := 50
-	// el prefijo & me da la direccion en memoria de a
-	b := &a
-
-	fmt.Println(b)
-	fmt.Println(*b)
-	// El prefijo * es el contrario al &, es para acceder
-	// al valor que la direccion tiene
-
-	*b = 100
-	fmt.Println(a)
-
-	myPC := pc{ram: 16, disk: 200, brand: "msi"}
+	myPC := pc{ram: 16, brand: "msi", disk: 100}
 	fmt.Println(myPC)
-
-	myPC.ping()
-
-	fmt.Println(myPC)
-	myPC.duplicateRAM()
-	fmt.Println(myPC)
-
 }
